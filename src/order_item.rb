194d73ae7510f32bb88126a23ec3abe01e1f6ba7
Joan Tiffany Siy <jsiy@freelancer.com>
2018-09-26 09:09:45 +0800

    Refactor processing bundles to get target order quantity

--- src/order_item.rb
+++ src/order_item.rb
@@ -27,72 +27,71 @@ class OrderItem
     return [] unless @bundles
     return [] if @quantity < @bundles.keys.min
 
-    select_best_match(matches)
+    find_exact_smallest_bundle(matches).map do |quantity, amount|
+      { bdl_qty: quantity, amount: amount, price: amount * @bundles[quantity] }
+    end
   end
 
   def matches
-    matches = []
-    @bundles.keys.each do |bdl_qty|
-      match = find_matches(
-        bdl_qty,
-        @quantity / bdl_qty,
-        @quantity % bdl_qty
-      )
-      matches.push(match) if match
-    end
-    matches
-  end
-
-  def find_matches(bdl_qty, amount, remainder, selected = [])
-    selected.push(
-      bdl_qty: bdl_qty,
-      amount: amount,
-      price: @bundles[bdl_qty]
-    )
-    return selected if remainder < @bundles.keys.min
-
-    @bundles.keys.each do |qty|
-      amount = remainder / qty
-      remainder = remainder % qty
-      selected.push(
-        bdl_qty: qty,
-        amount: amount,
-        price: @bundles[qty]
-      )
-
-      return selected if remainder < @bundles.keys.min
-
-      return find_matches(qty, amount, remainder, selected)
-    end
-  end
-
-  def select_best_match(matches)
-    best = nil
-    matches.each do |match|
-      match_total = 0
-      match_qty = 0
-      match.each do |bundle|
-        match_total += bundle[:bdl_qty] * bundle[:amount]
-        match_qty += bundle[:amount]
+    combos = []
+    # get all possible combinations of numbers to get target
+    @bundles.keys.each do |num|
+      partials = {}
+      partials[num] = @quantity / num
+      # stop if exact match to the target quantity already
+      if (@quantity % num).zero?
+        combos.push(compute_combo_info(partials))
+        next
       end
 
-      best ||= {
-        match: match,
-        total: match_total,
-        quantity: match_qty
-      }
-
-      exact_match = (match_total == @quantity)
-      less_bundle_count = (best[:total] != @quantity) &&
-                          (match_qty < best[:quantity])
-      next unless exact_match || less_bundle_count
-
-      best = {
-        match: match,
-        total: match_total,
-        quantity: match_qty
-      }
+      partials = process_subset(
+        @bundles.keys - partials.keys, @quantity, partials
+      )
+      combos.push(compute_combo_info(partials))
     end
-    best[:match]
+    combos
+  end
+
+  def compute_combo_info(partials)
+    {
+      bundles: partials,
+      sum: multiplier_sum(partials),
+      bundle_count: partials.values.reduce(0, :+)
+    }
+  end
+
+  def process_subset(numbers, target, partials = {})
+    return partials if numbers.empty?
+
+    sum = multiplier_sum(partials)
+    return partials if sum >= target
+
+    difference = target - sum
+    return partials if difference < numbers.min
+
+    numbers.each do |num|
+      partials[num] = difference / num
+      return process_subset(
+        numbers - partials.keys, target, partials
+      )
+    end
+  end
+
+  def multiplier_sum(values)
+    sum = 0
+    values.each do |quantity, multiplier|
+      sum += (quantity * multiplier)
+    end
+    sum
+  end
+
+  def find_exact_smallest_bundle(combos)
+    combos.sort_by! { |c| c[:bundle_count] }
+    # prioritize exact matches with the smallest bundle count
+    best = combos
+           .select { |c| c[:sum] == @quantity }
+           .first
+    best ||= combos.first
+    best[:bundles]
   end
 end
